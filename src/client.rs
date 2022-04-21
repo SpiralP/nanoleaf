12b20142b54fec6bc9de235d59e02b5699a6ba39
SpiralP <spiral.palmer@gmail.com>
2022-04-21 09:07:10 -0700

    fix effects api

--- src/client.rs
+++ src/client.rs
@@ -1,7 +1,7 @@
 use std::net::SocketAddr;
 
 use reqwest::{Body, ClientBuilder};
-use serde::{de::DeserializeOwned, Serialize};
+use serde::de::DeserializeOwned;
 use serde_json::json;
 use url::Url;
 
@@ -64,7 +64,8 @@ impl Client {
             NanoleafState::Off => false,
         };
         let on = On { value: val };
-        self.put_value(&format!("{}/state", token), "on", on).await
+        self.put_value(&format!("{}/state", token), json!({ "on": on }).to_string())
+            .await
     }
 
     // ====================
@@ -75,8 +76,11 @@ impl Client {
     }
 
     pub async fn set_brightness(&self, token: &str, brightness: Brightness) -> Result<()> {
-        self.put_value(&format!("{}/state", token), "brightness", brightness)
-            .await
+        self.put_value(
+            &format!("{}/state", token),
+            json!({ "brightness": brightness }).to_string(),
+        )
+        .await
     }
 
     // ====================
@@ -87,8 +91,11 @@ impl Client {
     }
 
     pub async fn set_hue(&self, token: &str, hue: SetRange) -> Result<()> {
-        self.put_value(&format!("{}/state", token), "hue", hue)
-            .await
+        self.put_value(
+            &format!("{}/state", token),
+            json!({ "hue": hue }).to_string(),
+        )
+        .await
     }
 
     // ====================
@@ -99,8 +106,11 @@ impl Client {
     }
 
     pub async fn set_saturation(&self, token: &str, sat: SetRange) -> Result<()> {
-        self.put_value(&format!("{}/state", token), "sat", sat)
-            .await
+        self.put_value(
+            &format!("{}/state", token),
+            json!({ "sat": sat }).to_string(),
+        )
+        .await
     }
 
     // ====================
@@ -111,7 +121,8 @@ impl Client {
     }
 
     pub async fn set_ct(&self, token: &str, ct: SetRange) -> Result<()> {
-        self.put_value(&format!("{}/state", token), "ct", ct).await
+        self.put_value(&format!("{}/state", token), json!({ "ct": ct }).to_string())
+            .await
     }
 
     // ====================
@@ -130,32 +141,44 @@ impl Client {
         self.get_value(&format!("{}/effects/select", token)).await
     }
 
+    pub async fn set_effect(&self, token: &str, effect: &str) -> Result<()> {
+        self.put_value(
+            &format!("{}/effects/select", token),
+            json!({ "select": effect }).to_string(),
+        )
+        .await
+    }
+
     pub async fn get_all_effects(&self, token: &str) -> Result<Vec<String>> {
         self.get_value(&format!("{}/effects/effectsList", token))
             .await
     }
 
-    pub async fn set_effect(&self, token: &str, effect: &str) -> Result<()> {
-        self.put_value(&format!("{}/effects", token), "select", effect.to_owned())
-            .await
+    pub async fn start_external_streaming(&self, token: &str) -> Result<()> {
+        self.put_value(
+            &format!("{}/effects", token),
+            json!({"write":{"command":"display","animType":"extControl","extControlVersion":"v2"}})
+                .to_string(),
+        )
+        .await
     }
 
     // ====================
     // Helpers
     // ====================
-    async fn get_value<T: DeserializeOwned>(&self, path: &str) -> Result<T> {
+    pub async fn get_value<T: DeserializeOwned>(&self, path: &str) -> Result<T> {
         let res = self.inner.get(path).await?;
         let res = res.error_for_status().map_err(Error::from)?;
         res.json::<T>().await.map_err(Error::from)
     }
 
-    async fn delete_value(&self, path: &str) -> Result<()> {
+    pub async fn delete_value(&self, path: &str) -> Result<()> {
         let res = self.inner.delete(path).await?;
         let res = res.error_for_status().map_err(Error::from)?;
         res.json::<()>().await.map_err(Error::from)
     }
 
-    async fn post_value<B, T>(&self, path: &str, body: B) -> Result<T>
+    pub async fn post_value<B, T>(&self, path: &str, body: B) -> Result<T>
     where
         T: DeserializeOwned,
         B: Into<Body>,
@@ -165,9 +188,8 @@ impl Client {
         res.json::<T>().await.map_err(Error::from)
     }
 
-    async fn put_value<T: Serialize>(&self, path: &str, key: &str, value: T) -> Result<()> {
-        let body = json!({ key: value });
-        let res = self.inner.put(path, body.to_string()).await?;
+    pub async fn put_value<B: Into<Body>>(&self, path: &str, body: B) -> Result<()> {
+        let res = self.inner.put(path, body).await?;
         res.error_for_status().map_err(Error::from)?;
 
         // API returns 204 No Content or a 4xx error only
